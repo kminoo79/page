55fffe088a7ab9103e3b1905edf0f076ed514b30
김민우 <kminoo79@gmail.com>
2021-12-04 09:10:41 +0000

    react, Express 이용 풀스텍 개발 공부

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# page
+# client
+	React FE 서비스
+ 
+# server
+	Express BE 서비스
+	
+# 명령어
+	
+	client 터미널 : npm build - 빌드후 서버 실행 해야 react앱이 연동됨
+	
+	server 터미널 : nodemon server - 서버 실시간 수정 적용 실행
---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const http = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.get('/hello', (req, res) => res.send('Hello World!'))
+
+http.listen(80, function () {
+  console.log('listening on 80')
+}); 
